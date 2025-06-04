5b80cbc5eb9a1ce59b005eb0f857ffccf637ea05
Devin Schumacher <45643901+devinschumacher@users.noreply.github.com>
2025-06-04 05:21:22 -0700

    11 lander (#51)
    resolves #11

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl">
-      hello tailwind
-    </h1>
-    <button class="btn btn-secondary">
-      View map <Icon name="tabler:map" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
